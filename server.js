c625cc0047350b1ef3c075958c46f596be5a5b69
bgeun <sskm0116@naver.com>
2024-04-09 18:12:04 +0900

    api key hide

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const dotenv = require('dotenv');
 
-app.listen(8080, function(){
-    console.log('listening on 8080');
+dotenv.config();
+
+// 정적 파일 제공을 위한 미들웨어를 추가합니다.
+app.use(express.static(path.join(__dirname)));
+
+// '/api/keys' 경로에 대한 GET 요청을 처리합니다.
+app.get('/api/keys', (req, res) => {
+  res.json({ apiKeyMap: process.env.OPENAPI_KEY_MAP,
+     apiKeyRealTimeTraffic: process.env.OPENAPI_KEY_REALTIMETRAFFIC });
 });
 
-app.get('/', function(요청, 응답){
-    응답.sendfile(__dirname + '/index.html');
+// '/' 경로에 대한 GET 요청을 처리합니다.
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/map', function(요청, 응답){
-    응답.sendfile(__dirname + '/map.html');
+// 서버를 8080 포트에서 시작합니다.
+app.listen(8080, function() {
+  console.log('Server is listening on port 8080');
 });
