afb11eb11b90ea97fa279cbc26a80bed4c525a01
송봉근 <sskm0116@naver.com>
2024-05-27 16:35:39 +0900

    네이버 지도 api로 대체

--- server.js
+++ server.js
@@ -8,18 +8,15 @@ dotenv.config();
 // 정적 파일 제공을 위한 미들웨어를 추가합니다.
 app.use(express.static(path.join(__dirname)));
 
-// '/api/keys' 경로에 대한 GET 요청을 처리합니다.
-app.get('/api/keys', (req, res) => {
-  res.json({ 
-    apiKeyMap: process.env.OPENAPI_KEY_MAP, 
-    apiKeyRealTimeTraffic: process.env.OPENAPI_KEY_REALTIMETRAFFIC 
-  });
-});
-
 // '/' 경로에 대한 GET 요청을 처리합니다.
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/map', function(req, res) {
+  res.sendFile(__dirname + '/map.html');
+});
+
 
 // 서버를 8080 포트에서 시작합니다.
 app.listen(8080, function() {
